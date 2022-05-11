2216f691e5168ba165f28e17bad631994e19a622
Passenger89 <nicho268@gmail.com>
2022-05-11 23:29:55 +0100

    add mobile navigation

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,32 @@ const resizeListener = () => {
 window.onload = resizeListener()
 window.addEventListener('resize', resizeListener)
 /* ------ */
+
+/* ----- TOGGLE MOBILE NAVIGATION ----- */
+const header = document.querySelector('.header')
+const burgerBtn = document.querySelector('#btnHamburger')
+const overlay = document.querySelector('.header__mobile-overlay')
+const mobileMenu = document.querySelector('.header__nav--mobile')
+
+burgerBtn.addEventListener(
+  'click',
+  () => {
+    header.classList.toggle('open')
+    if (header.classList.contains('open')) {
+      //* Open hamburger menu
+      overlay.classList.remove('fade-out')
+      overlay.classList.add('fade-in')
+      mobileMenu.classList.remove('slide-right')
+      mobileMenu.classList.add('slide-left')
+    } else {
+      //* Close hamburger menu
+      overlay.classList.remove('fade-in')
+      overlay.classList.add('fade-out')
+      mobileMenu.classList.remove('slide-left')
+      mobileMenu.classList.add('slide-right')
+    }
+  },
+  false
+)
+
+/* ------ */
