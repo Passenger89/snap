beb39a0a2b98a7fb194f97612e6ed70fd4d6d069
Passenger89 <nicho268@gmail.com>
2022-05-13 00:30:32 +0100

    update css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,9 @@
 const heroImg = document.querySelector('#hero-img')
 const resizeListener = () => {
   heroImg.src =
-    window.innerWidth > 600
-      ? './images/image-hero-desktop.png'
-      : './images/image-hero-mobile.png'
+    window.innerWidth > 780
+      ? './src/images/image-hero-mobile.png'
+      : './src/images/image-hero-desktop.png'
 }
 window.onload = resizeListener()
 window.addEventListener('resize', resizeListener)
